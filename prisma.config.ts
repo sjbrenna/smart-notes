c2eb087d771a0f5a27b8cc422955805a6aac9798
sjbrenna <samuel.jhbrenna@gmail.com>
2026-05-31 19:03:12 +0200

    prisma folder fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/generated/schema.prisma",
+  schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
